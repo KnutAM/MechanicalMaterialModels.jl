e514ca5a721d7b118f1bd8bdb7669669e7ee3fa2
Knut Andreas <knutam@gmail.com>
2025-03-20 14:43:22 +0100

    Add GeneralizedMaxwell viscoelastic model

---
+++ src/ViscoElastic.jl
@@ -0,0 +1,73 @@
+"""
+    Maxwell(; G, η, t)
+
+Create a maxwell chain element with shear stiffness `G`. Either the viscosity, `η`, 
+or the relaxation time, `t = η / G`, should be supplied (not both).
+"""
+struct Maxwell{T}
+    G::T
+    η::T
+end
+function Maxwell(; G = nothing, η = nothing, t = nothing)
+    G === nothing && throw(ArgumentError("G must be given"))
+    if η === nothing
+        t === nothing && throw(ArgumentError("Either t or η must be given, but not both"))
+        return Maxwell(G, G * t)
+    else
+        t !== nothing && throw(ArgumentError("Either t or η must be given, but not both"))
+        return Maxwell(G, η)
+    end
+end
+
+function calculate_viscous_strain(m::Maxwell, ϵ::SymmetricTensor, ϵv_old::SymmetricTensor, Δt::Number)
+    # ϵv - ϵv_old = Δt * σv / m.η
+    # σv = 2G * (ϵdev - ϵv)
+    # ϵv * (1 + Δt * 2G / m.η) = ϵv_old +  Δt * 2G * ϵdev / m.η
+    Δt_2G_div_η = Δt * 2 * m.G / m.η
+    return (ϵv_old +  Δt_2G_div_η * dev(ϵ)) / (1 + Δt_2G_div_η)
+end
+
+function calculate_stress(m::Maxwell, ϵ::SymmetricTensor, ϵv_old::SymmetricTensor, Δt::Number)
+    ϵv = calculate_viscous_strain(m, ϵ, ϵv_old, Δt)
+    return 2 * m.G * (dev(ϵ) - ϵv)
+end
+
+"""
+    GeneralizedMaxwell(elastic::LinearElastic, chains::Maxwell...)
+
+Create a generalized Maxwell model with an arbitrary number of Maxwell chains.
+The `elastic` part refers to the long-term stiffness contribution. 
+
+!!! note "WIP"
+    The `GeneralizedMaxwell` and `Maxwell` are currently quite specific to isotropic behavior,
+    this may change in the future with related changes to the constructors.
+
+"""
+struct GeneralizedMaxwell{ET, T, num} <: AbstractMaterial
+    base::ET
+    chains::NTuple{num, Maxwell{T}}
+end
+
+function GeneralizedMaxwell(base::LinearElastic, chains::Maxwell...)
+    return GeneralizedMaxwell(base, chains)
+end
+
+struct GeneralizedMaxwellState{T, num} <: AbstractMaterialState
+    ϵv::NTuple{num, SymmetricTensor{2,3,T,6}}
+end
+
+function MMB.initial_material_state(::GeneralizedMaxwell{<:Any, <:Any, num}) where {num}
+    T = Float64 # temp
+    return GeneralizedMaxwellState(ntuple(_ -> zero(SymmetricTensor{2, 3, T}), num))
+end
+
+function calculate_stress(m::GeneralizedMaxwell, ϵ::SymmetricTensor, old::GeneralizedMaxwellState, Δt)
+    σ0 = calculate_stress(m.base, ϵ)
+    return mapreduce((c, ϵv) -> calculate_stress(c, ϵ, ϵv, Δt), +, m.chains, old.ϵv; init = σ0)
+end
+
+function MMB.material_response(m::GeneralizedMaxwell, ϵ::SymmetricTensor{2,3}, old::GeneralizedMaxwellState, Δt, _, _)
+    dσdϵ, σ = gradient(e -> calculate_stress(m, e, old, Δt), ϵ, :all)
+    state = GeneralizedMaxwellState(map((c, ϵv_old) -> calculate_viscous_strain(c, ϵ, ϵv_old, Δt), m.chains, old.ϵv))
+    return σ, dσdϵ, state
+end
---
+++ test/test_viscoelastic.jl
@@ -0,0 +1,52 @@
+@testset "Viscoelasticity" begin
+    G0 = 10 * (1 + rand())
+    K0 = 20 * (1 + rand())
+    Gs = ntuple(_ -> G0 * (0.5 + rand()), 5)         # Random chain stiffness around G0
+    ts = ntuple(i -> (1 + rand()) * 10.0 ^ (i - 2), 5) # Relaxation times ~ 0.01, 0.1, 1, 10, 100
+    E0 = 9 * G0 * K0 / (3 * K0 + G0)
+    ν0 = E0 / (2 * G0) - 1
+    me = LinearElastic(;E = E0, ν = ν0)
+    chains = map((G, t) -> Maxwell(;G, t), Gs, ts)
+
+    m1 = GeneralizedMaxwell(me, chains[1])
+    m2 = GeneralizedMaxwell(me, chains[1:2])
+    m5 = GeneralizedMaxwell(me, chains)
+
+    function instant_stiffness(m::GeneralizedMaxwell)
+        I2 = one(SymmetricTensor{2,3})
+        I4dev = one(SymmetricTensor{4,3}) - I2 ⊗ I2 / 3
+        return m.base.C + sum(c -> 2 * c.G, m.chains) * I4dev
+    end
+    relaxation_time(m::Maxwell) = m.η / m.G
+    relaxation_time(m::GeneralizedMaxwell) = maximum(relaxation_time, m.chains)
+    
+    # Instantanious and long-term stiffness responses
+    D0 = me.C
+    @testset "GM (num = $(length(m.chains)))" for m in (m1, m2, m5)
+        Δϵ = rand(SymmetricTensor{2,3}) / 100
+        state = initial_material_state(m)
+        # Instant stiffness
+        σ, dσdϵ, _ = material_response(m, Δϵ, state, 1e-20)
+        @test dσdϵ ≈ instant_stiffness(m)
+        @test σ ≈ instant_stiffness(m) ⊡ Δϵ
+        # Long-term secant stiffness
+        σ, dσdϵ, _ = material_response(m, Δϵ, state, 1e20)
+        @test dσdϵ ≈ D0
+        @test σ ≈ D0 ⊡ Δϵ
+
+        # Consider load stepping and check that we get the correct saturated stress level 
+        T_max = 20 * relaxation_time(m)
+        n_steps = 100
+        t_old = 0
+        state = initial_material_state(m)
+        ϵ_end = rand(SymmetricTensor{2,3}) / 100
+        for i in 1:n_steps
+            ϵ = clamp(i/10, 0, 1) * ϵ_end
+            t = T_max * (i / n_steps) ^ 2
+            Δt = t - t_old
+            σ, _, state = material_response(m, ϵ, state, Δt)
+            t_old = t
+        end
+        @test σ ≈ D0 ⊡ ϵ_end
+    end
+end
