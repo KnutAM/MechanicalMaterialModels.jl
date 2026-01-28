0ece848a56d43e9219d47d1a431be4e625a6ef15
Jacob Gunnarsson <jacob.gunnarsson@chalmers.se>
2026-01-28 17:16:06 +0100

    Changed SainVenant to be a wrapper around LinearElastic and added test

--- src/hyper_elasticity/SaintVenant.jl
+++ src/hyper_elasticity/SaintVenant.jl
@@ -9,13 +9,13 @@ The-Saint Venant formulation with shear modulus `G` and bulk modulus `K` defined
 where ``\\boldsymbol{E}`` is the Green-Lagrange strain tensor. 
 
 """
-@kwdef struct SaintVenant{T} <: AbstractHyperElastic
-    G::T
-    K::T
+@kwdef struct SaintVenant{E} <: AbstractHyperElastic
+    elastic::E
 end
 
 function compute_potential(m::SaintVenant, C::SymmetricTensor)
+    println("Using SaintVenant model")
     E = 0.5 * (C - one(C))
-    return m.G * dev(E)⊡dev(E)+m.K/2 * (tr(E))^2
+    return 1/2 * E ⊡ m.elastic.C ⊡ E
 end
 MMB.get_vector_eltype(::SaintVenant{T}) where {T} = T
